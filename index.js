fe2ba01766aa07122977d1f8201542f51c322963
Tanveer <tanveertnk11@gmail.com>
2019-12-16 15:56:23 +0700

    configured ports to avoid collision between processes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const {isUser, saveMessage} = require('./db/db');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {

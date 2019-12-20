bd49babc53a3bf6a7eb524b07c46f06f7a26745b
Tanveer Singh <57829318+tanveertnk11@users.noreply.github.com>
2019-12-20 11:05:27 +0700

    update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -13,25 +13,39 @@ async function isUser (id) {
 }
 
 //Save Message in the DB
-async function saveMessage (userId, userMessage) {
+async function saveMessage (userId, userName, userMessage) {
     try {
-        const query = `INSERT INTO chat(chatType,userId,userMessage) VALUES(2,"${userId}","${userMessage}")`;
+        const query = `INSERT INTO chat(userId,userName,userMessage) VALUES("${userId}","${userName}",${db.escape(userMessage)})`;
         await db.execute(query);
     } catch (error) {
         console.log('Unable to insert messages in the database');
     }
 }
 
+//Fetch all messages of global chat
 async function getAllMessages () {
     try {
-        const query = `SELECT * FROM chat`;
-        const messages = await db.execute(query);
-        console.log(messages[0]);
-        return messages[0.
-        ];
+        const query = `SELECT * FROM chat where chatType=0`;
+        const data = await db.execute(query);
+        const messages = formatMesasage(data[0]);
+        return messages;
     } catch (error) {
         console.log('Unable to fetch messages');
     }
+}
+
+//Format message
+function formatMesasage(messages) {
+    let temp = [];
+    for(let i=0; i<messages.length; i++) {
+        let message = {
+            userId: messages[i].userId,
+            name: messages[i].userName,
+            message: messages[i].userMessage
+        };
+        temp.push(message);
+    }
+    return {status: true, data: temp};
 }
 
 module.exports = {
